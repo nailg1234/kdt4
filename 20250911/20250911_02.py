7deedb76975ef0df7735bc57de2d62a9ff4a6109
nailg1234 <nailg@naver.com>
2025-09-11 13:10:47 +0900

    20250911 commit
    commit

---
+++ 20250911/20250911_02.py
@@ -0,0 +1,27 @@
+# a = 10
+# if a == 10:
+#     print(f'a : {a}')
+#     pass
+#     print(f'a ~~~')
+
+
+age = 20
+if age >= 18:
+    print('성인입니다.')
+
+name = "gy"
+name = ""
+
+if name:
+    print('이름이 존재 합니다.')
+
+if True:
+    print('무조건 실행')
+
+if False:
+    print('실행되지 않음')
+
+if True:
+    pass  # 다음에 작성하겠다
+
+print('조건문과 상관없습니다.')
