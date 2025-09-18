71b099ff548e7e728a3d8d13b4adcb4c0d290234
nailg1234 <nailg@naver.com>
2025-09-18 17:06:14 +0900

    올바른 괄호 주석 수정

--- programmers/stack/stack_02.py
+++ programmers/stack/stack_02.py
@@ -24,11 +24,11 @@ def solution(s):
         while len(s_list):
             pop_value = s_list.pop()
             if pop_value == ')': # ')'
-                close_stack_list.append(pop_value) # ')' 쌓기
+                close_stack_list.append(pop_value) # ')' close_stack.push()
                 
             else: # '('
                 if len(close_stack_list): # close_stack에 요소 있는 경우
-                    close_stack_list.pop() # close_stack에 ')' 1개 제거
+                    close_stack_list.pop() # ')' close_stack.pop()
                     
                 else:
                     return False # close_stack에 요소가 없는 경우 짝이 맞지 않으므로 return False
