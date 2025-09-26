b5e8821cd24489623c391cb08669b43460fb78e4
nailg1234 <nailg@naver.com>
2025-09-26 20:30:48 +0900

    프로젝트 저장소 따로 관리

--- Python/project/PY-GAME/battle/__init__.py
+++
@@ -1,5 +0,0 @@
-#.battle_manager 현재 패키지 기준으로 battle_manager.py 내부의 함수 start_battle 임포트
-from .battle_manager import start_battle, create_character
-
-# __all__ : game.py 파일에서 from battle import * 임포트시 접근 할 수 있는 이름들 목록
-__all__ = ['start_battle', 'create_character']
--- Python/project/PY-GAME/utils/helpers.py
+++
@@ -1,9 +0,0 @@
-import random
-
-# 1 ~ 10 중에 8, 9, 10이 나오면 True (30% 확률로 특수 공격) 
-def is_special_attack():
-    return 7 < random.randint(1, 10)
-
-# 1 ~ 10 중에 4, 5, 6, 7, 8, 9, 10이 나오면 True (70% 확률로 도적의 특수 공격시전) 
-def is_special_attack_rogue():
-    return 3 < random.randint(1, 10)
