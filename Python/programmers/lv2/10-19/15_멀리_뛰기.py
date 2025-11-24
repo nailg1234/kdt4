aca7d722043b446ee033e45d92225d0ff2a869e0
nailg1234 <nailg@naver.com>
2025-11-24 14:41:02 +0900

    프로그래머스 멀리뛰기

--- Python/programmers/lv2/10-19/15_멀리_뛰기.py
+++ Python/programmers/lv2/10-19/15_멀리_뛰기.py
@@ -1,3 +1,15 @@
 def solution(n):
-    answer = 0
-    return answer
+    # DP 배열 생성 (0번 인덱스 부터 사용)
+    dp = [0] * (n + 1)
+    
+    # 기본값 설정
+    dp[0] = 1
+    dp[1] = 1
+    if n >= 2:
+        dp[2] = 2
+    
+    # 피보나치 형태로 개선
+    for i in range(3, n + 1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567 
+    
+    return dp[n]
