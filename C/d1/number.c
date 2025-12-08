8da8272f65a0b5e98dc3e1dbc6fd97152f631999
nailg1234 <nailg@naver.com>
2025-12-08 15:14:04 +0900

    버려

--- C/d1/number.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-static void number() {
-	printf("%d은 정수입니다\n", 10);
-	printf("%d은 정수이다\n", 10);
-	printf("%.2f은 실수이다\n", 3.14);
-	printf("%c는 첫번째 글자입니다.\n", 'A');
-	printf("%s는 문자열이다.\n", "Hey");
-}
