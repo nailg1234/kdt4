bf7971d2b2cd3176775f35b3d67a3da00d657a8a
nailg1234 <nailg@naver.com>
2025-12-11 13:56:57 +0900

    아두이노 D6 와이파이

---
+++ Arduino/19_Wifi_01/19_Wifi_01.ino
@@ -0,0 +1,18 @@
+#include <SoftwareSerial.h>
+SoftwareSerial myESP(2, 3);
+// myESP 객체를 만들 때 전달한 매개변수 2개는 각 RX와 TX
+
+void setup() {
+  Serial.begin(9600);
+  myESP.begin(9600);
+}
+
+void loop() {
+  if(myESP.available()) { // 아두이노와 ESP-01이 잘 연결 되었다면
+    Serial.write(myESP.read()); // ESP-01이 PC로 어떤 것을 출력하는 코드
+  }
+
+  if(Serial.available()) { // 아두이노와 ESP-01이 잘 연결 되었다면
+    myESP.write(Serial.read()); // PC가 ESP-01로 어떤 것을 출력하는 코드
+  }
+}
